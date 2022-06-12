03f012610eaed79b95e65a2f6f2cc839ceb66f72
Burak <57848751+Likeslearning@users.noreply.github.com>
2022-06-12 10:43:12 +0300

    Add files via upload

---
+++ ENGRProject/index.php
@@ -0,0 +1,15 @@
+<?php
+   include("menu.php");
+?>
+<body class="maincontent">
+    <div  style="font-size: 45px; margin-top: 10px;" align="center">
+        Welcome to our unit conversion site!<br>
+        Many unit conversion types exist!<br>
+ 
+        <img src="balance.gif" align="center" style="image-rendering: -webkit-optimize-contrast; width: 800px; height:500px; margin-top: 10px;"/>
+    </div>
+</body>
+
+
+
+
