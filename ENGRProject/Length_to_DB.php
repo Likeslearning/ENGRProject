e38ad494ce2ed2c7f183ed6e3b9afa163a11f233
Burak <57848751+Likeslearning@users.noreply.github.com>
2022-06-13 14:46:24 +0300

    Add files via upload

---
+++ ENGRProject/Length_to_DB.php
@@ -0,0 +1,22 @@
+<?php
+
+$user_input = $_POST["value2"];
+$from = $_POST["from2"];
+$to = $_POST["to2"];
+$output = $_POST["result2"];
+
+$conn = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b494fa8588b593", "9868e4e7", "heroku_f3d0fe302c6dbfc");
+
+if(!$conn) {
+    die('Problem about database connection');
+}
+
+if($user_input != "" && $from != "" && $to != "" && $output != "") {
+    $sql = "insert into length(user_value, from_unit, to_unit, converted_value) values('".$user_input."', '".$from."', '".$to."', '".$output."')";
+    $result = mysqli_query($conn, $sql);
+}
+            
+else {}
+
+
+
